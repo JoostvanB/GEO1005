646ebe76469b2784112c945d3f8b55bae2392d92
juangl77 <juampis77@gmail.com>
2018-01-15 16:43:03 +0100

    Joining everythimng again

--- SpatialDecision/spatial_decision_dockwidget.py
+++ SpatialDecision/spatial_decision_dockwidget.py
@@ -32,12 +32,19 @@ import processing
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+from matplotlib.spines import Spine
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.projections import register_projection
+
 # Initialize Qt resources from file resources.py
 import resources
 
 import os
 import os.path
 import random
+import numpy as np
 import csv
 import time
 
@@ -58,9 +65,14 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS, QgsMapTool):
     def __init__(self, iface, parent=None):
         """Constructor."""
         super(SpatialDecisionDockWidget, self).__init__(parent)
-        os.chdir(os.path.join(os.path.dirname(__file__),'QGIS files'))
+        if os.path.isdir(os.path.join(os.path.dirname(__file__),'QGIS files')):
+            os.chdir(os.path.join(os.path.dirname(__file__),'QGIS files'))
+        else:
+            os.mkdir(os.path.join(os.path.dirname(__file__),'QGIS files'))
+            os.chdir(os.path.join(os.path.dirname(__file__), 'QGIS files'))
         self.workingDirectory = os.getcwd()
         print("The working directory is "+str(self.workingDirectory))
+        self.initiated = False
         # Set up the user interface from Designer.
         # After setupUI you can access any designer object by doing
         # self.<objectname>, and you can use autoconnect slots - see
@@ -75,6 +87,7 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS, QgsMapTool):
         self.Demand = [0, 0, 0, 0]
         # set up GUI operation signals
         # data
+
         self.loadDataButton.clicked.connect(self.loadData)
         #self.saveScenarioButton.clicked.connect(self.saveScenario)
         self.checkBoxDemand.stateChanged.connect(lambda: self.updateLayers(self.checkBoxDemand.text(),self.checkBoxDemand.isChecked()))
@@ -85,56 +98,72 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS, QgsMapTool):
         self.checkBoxWaterways.stateChanged.connect(lambda: self.updateLayers(self.checkBoxWaterways.text(), self.checkBoxWaterways.isChecked()))
         self.checkBoxRailways.stateChanged.connect(lambda: self.updateLayers(self.checkBoxRailways.text(), self.checkBoxRailways.isChecked()))
         self.createScenarioButton.clicked.connect(self.createScenario)
-        #self.pushButtonAdd.clicked.connect(self.selectClickTool)
-        #self.pushButtonConfirmPoint.clicked.connect(self.enterPoi)
+        self.deleteScenarioButton.clicked.connect(self.deleteScenario)
+        self.turnVisibilityButton.clicked.connect(self.turnVisibility)
+        self.addPointsButton.clicked.connect(self.selectClickTool)
+        self.confirmPointsButton.clicked.connect(self.enterPoi)
         # analysis
         self.emitPoint = QgsMapToolEmitPoint(self.canvas)
         self.emitPoint.canvasClicked.connect(self.getPoint)
         self.pushButton.clicked.connect(self.getData)
 
         # initialisation
-        #self.updateLayers()
+        self.loadData()
 
-        #run simple tests
+        #run figure
+
+
 #######
 #   Overview functions
 #######
     #This method loads the project which includes background data and scenarios
-    def loadData(self,filename=""):
-        scenario_open = False
-        scenario_file = "Week 5 project 2.0.qgs"
-        print(scenario_file)
-        # check if file exists
-        if os.path.isfile(scenario_file):
-            self.iface.addProject(scenario_file)
-            scenario_open = True
-        else:
-            last_dir = uf.getLastDir("SpatialDecision")
-            new_file = QtGui.QFileDialog.getOpenFileName(self, "", last_dir, "(*.qgs)")
-            if new_file:
-                self.iface.addProject(unicode(new_file))
-                scenario_open = True
+    def loadData(self):
+        new_file = False
+        if not self.initiated:
+            scenario_file = "Week 5 project 2.0.qgs"
+            print(scenario_file)
+            # check if file exists
+            if os.path.isfile(scenario_file):
+                self.iface.addProject(scenario_file)
+            else:
+                last_dir = uf.getLastDir("SpatialDecision")
+                new_file = QtGui.QFileDialog.getOpenFileName(self, "", last_dir, "(*.qgs)")
+                if new_file:
+                    self.iface.addProject(unicode(new_file))
+
         root = QgsProject.instance().layerTreeRoot()
         found = False
-
+        legend = self.iface.legendInterface()
         for child in root.children():
             if isinstance(child, QgsLayerTreeGroup):
                 if child.name() == "Scenarios":
                     found = True
                     parent = child
                     if parent.children():
+                        count = 0
                         for child in parent.children():
-                            self.activeScenarioCombo.addItem(child.name())
-                            self.activeScenarioCombo_2.addItem(child.name())
-        print(found)
+                            if not self.initiated or new_file:
+                                tempLayer = uf.getLegendLayerByName(self.iface, child.name())
+                                self.activeScenarioCombo_2.addItem(child.name())
+                                self.activeScenarioCombo_2.setCurrentIndex(0)
+                                if legend.isLayerVisible(tempLayer):
+                                    self.activeScenarioCombo_2.model().item(count).setForeground(QtGui.QColor('green'))
+                                    print("green")
+                                else:
+                                    self.activeScenarioCombo_2.model().item(count).setForeground(QtGui.QColor('red'))
+                                count += 1
+
+
+
         if not found:
             scenarioGroup = root.insertGroup(0, "Scenarios")
-        layers = uf.getLegendLayers(self.iface, 'all', 'all')
+        self.initiated = True
+
+
     def createScenario(self):
         nameScenario, okPressed = QtGui.QInputDialog.getText(self, "Create Scenario", "Please input the new scenario name:")
         if okPressed:
-            self.activeScenarioCombo.addItem(nameScenario)
-            self.activeScenarioCombo_2.addItem(nameScenario)
+
             newLayer = QgsVectorLayer("Point", nameScenario, "memory")
             layerData = newLayer.dataProvider()
             layerData.addAttributes([QgsField("ID", QVariant.String), QgsField("lat", QVariant.String),
@@ -144,7 +173,35 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS, QgsMapTool):
             root = QgsProject.instance().layerTreeRoot()
             parentGroup = root.findGroup("Scenarios")
             QgsMapLayerRegistry.instance().addMapLayer(newLayer, False)
-            parentGroup.insertChildNode(0, QgsLayerTreeLayer(newLayer))
+            parentGroup.insertChildNode(0,QgsLayerTreeLayer(newLayer))
+
+            self.activeScenarioCombo_2.insertItem(0, nameScenario)
+            self.activeScenarioCombo_2.setCurrentIndex(0)
+            legend = self.iface.legendInterface()
+            if legend.isLayerVisible(newLayer):
+                self.activeScenarioCombo_2.model().item(0).setForeground(QtGui.QColor('green'))
+            else:
+                self.activeScenarioCombo_2.model().item(0).setForeground(QtGui.QColor('red'))
+
+    def deleteScenario(self):
+        layerName = self.activeScenarioCombo_2.currentText()
+        layerIndex = self.activeScenarioCombo_2.currentIndex()
+        layer = uf.getLegendLayerByName(self.iface, layerName)
+        if layer:
+            QgsMapLayerRegistry.instance().removeMapLayer(layer.id())
+            self.activeScenarioCombo_2.removeItem(layerIndex)
+            self.activeScenarioCombo_2.setCurrentIndex(0)
+    def turnVisibility(self):
+        index = self.activeScenarioCombo_2.currentIndex()
+        text = self.activeScenarioCombo_2.currentText()
+        layer = uf.getLegendLayerByName(self.iface, text)
+        if layer:
+            legend = self.iface.legendInterface()
+            legend.setLayerVisible(layer, not legend.isLayerVisible(layer))
+            if legend.isLayerVisible(layer):
+                self.activeScenarioCombo_2.model().item(int(index)).setForeground(QtGui.QColor('green'))
+            else:
+                self.activeScenarioCombo_2.model().item(int(index)).setForeground(QtGui.QColor('red'))
 
     def saveScenario(self):
         self.iface.actionSaveProject()
@@ -211,6 +268,152 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS, QgsMapTool):
 #######
 #    Analysis functions
 #######
+
+    #Plot analysis
+    def plotAnalysis(self):
+        def radar_factory(num_vars, frame='circle'):
+
+            # calculate evenly-spaced axis angles
+            theta = np.linspace(0, 2 * np.pi, num_vars, endpoint=False)
+            # rotate theta such that the first axis is at the top
+            theta += np.pi / 2
+
+            def draw_poly_patch(self):
+                verts = unit_poly_verts(theta)
+                return plt.Polygon(verts, closed=True, edgecolor='k')
+
+            def draw_circle_patch(self):
+                # unit circle centered on (0.5, 0.5)
+                return plt.Circle((0.5, 0.5), 0.5)
+
+            patch_dict = {'polygon': draw_poly_patch, 'circle': draw_circle_patch}
+            if frame not in patch_dict:
+                raise ValueError('unknown value for `frame`: %s' % frame)
+
+            class RadarAxes(PolarAxes):
+
+                name = 'radar'
+                # use 1 line segment to connect specified points
+                RESOLUTION = 1
+                # define draw_frame method
+                draw_patch = patch_dict[frame]
+
+                def fill(self, *args, **kwargs):
+                    """Override fill so that line is closed by default"""
+                    closed = kwargs.pop('closed', True)
+                    return super(RadarAxes, self).fill(closed=closed, *args, **kwargs)
+
+                def plot(self, *args, **kwargs):
+                    """Override plot so that line is closed by default"""
+                    lines = super(RadarAxes, self).plot(*args, **kwargs)
+                    for line in lines:
+                        self._close_line(line)
+
+                def _close_line(self, line):
+                    x, y = line.get_data()
+                    # FIXME: markers at x[0], y[0] get doubled-up
+                    if x[0] != x[-1]:
+                        x = np.concatenate((x, [x[0]]))
+                        y = np.concatenate((y, [y[0]]))
+                        line.set_data(x, y)
+
+                def set_varlabels(self, labels):
+                    self.set_thetagrids(np.degrees(theta), labels)
+
+                def _gen_axes_patch(self):
+                    return self.draw_patch()
+
+                def _gen_axes_spines(self):
+                    if frame == 'circle':
+                        return PolarAxes._gen_axes_spines(self)
+                    # The following is a hack to get the spines (i.e. the axes frame)
+                    # to draw correctly for a polygon frame.
+
+                    # spine_type must be 'left', 'right', 'top', 'bottom', or `circle`.
+                    spine_type = 'circle'
+                    verts = unit_poly_verts(theta)
+                    # close off polygon by repeating first vertex
+                    verts.append(verts[0])
+                    path = Path(verts)
+
+                    spine = Spine(self, spine_type, path)
+                    spine.set_transform(self.transAxes)
+                    return {'polar': spine}
+
+            register_projection(RadarAxes)
+            return theta
+
+        def unit_poly_verts(theta):
+            x0, y0, r = [0.5] * 3
+            verts = [(r * np.cos(t) + x0, r * np.sin(t) + y0) for t in theta]
+            return verts
+
+        def example_data():
+            # The following data is from the Denver Aerosol Sources and Health study.
+            # See  doi:10.1016/j.atmosenv.2008.12.017
+            #
+            # The data are pollution source profile estimates for five modeled
+            # pollution sources (e.g., cars, wood-burning, etc) that emit 7-9 chemical
+            # species. The radar charts are experimented with here to see if we can
+            # nicely visualize how the modeled source profiles change across four
+            # scenarios:
+            #  1) No gas-phase species present, just seven particulate counts on
+            #     Sulfate
+            #     Nitrate
+            #     Elemental Carbon (EC)
+            #     Organic Carbon fraction 1 (OC)
+            #     Organic Carbon fraction 2 (OC2)
+            #     Organic Carbon fraction 3 (OC3)
+            #     Pyrolized Organic Carbon (OP)
+            #  2)Inclusion of gas-phase specie carbon monoxide (CO)
+            #  3)Inclusion of gas-phase specie ozone (O3).
+            #  4)Inclusion of both gas-phase species is present...
+            data = [
+                ['',
+                 'Average household income',
+                 'Distance to closest Point of Interest',
+                 'Number of Points of Interest within 200m radius',
+                 '',
+                 'Average occupation of closest charging spot',
+                 'Demand of parking spots',
+                 'Distance to closest charging spot'],
+
+                ('First potential spot', [
+                    [0.00, 0.81, 0.96, 0.83, 0.00, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.00, 0.8, 0.9, 0.8]]),
+                ('Second potential spot', [
+                    [0.00, 0.41, 0.7, 0.63, 0.00, 0.0, 0.0, 0.0],
+                    [0.0, 0.0, 0.0, 0.0, 0.00, 0.5, 0.9, 0.7]]),
+
+            ]
+            return data
+
+
+        N = 8
+        theta = radar_factory(N, frame='polygon')
+
+        data = example_data()
+        spoke_labels = data.pop(0)
+
+        fig, axes = plt.subplots(figsize=(20, 20), nrows=2, ncols=1,
+                                 subplot_kw=dict(projection='radar'))
+        fig.subplots_adjust(wspace=0.55, hspace=0.20, top=0.85, bottom=0.05)
+
+        colors = ['r', 'b']
+        for ax, (title, case_data) in zip(axes.flatten(), data):
+            ax.set_rgrids([0.2, 0.4, 0.6, 0.8], angle=90)
+            ax.set_title(title, weight='bold', size='large', position=(0.5, 1.1),
+                         horizontalalignment='center', verticalalignment='center')
+            for d, color in zip(case_data, colors):
+                ax.plot(theta, d, color=color)
+                ax.fill(theta, d, facecolor=color, alpha=0.65)
+            ax.set_varlabels(spoke_labels)
+
+        fig.text(0.5, 0.68, 'Comparing chosen spots',
+                 horizontalalignment='center', color='green', weight='bold',
+                 size='xx-large')
+
+        fig.savefig('puta.png')
     # route functions
 
     def getNetwork(self):
@@ -423,50 +626,36 @@ class SpatialDecisionDockWidget(QtGui.QDockWidget, FORM_CLASS, QgsMapTool):
 
     # get the point when the user clicks on the canvas
     def enterPoi(self):
-        # remember currently selected tool
-        self.npuntos = (self.npuntos + 1)%4
-        # activate coordinate capture tool
         self.canvas.setMapTool(self.userTool)
     def selectClickTool(self):
         self.canvas.setMapTool(self.emitPoint)
 
     def getData(self):
-        return None
+        print("Nice Click")
+
     def getPoint(self, mapPoint, mouseButton):
-        # change tool so you don't get more than one POI
-        if self.npuntos == 0:
-            self.lineEditOneLat.setText(str(mapPoint.x()))
-            self.lineEditOneLon.setText(str(mapPoint.y()))
-        elif  self.npuntos == 1:
-            self.lineEditTwoLat.setText(str(mapPoint.x()))
-            self.lineEditTwoLon.setText(str(mapPoint.y()))
-        elif self.npuntos == 2:
-            self.lineEditThreeLat.setText(str(mapPoint.x()))
-            self.lineEditThreeLon.setText(str(mapPoint.y()))
-        elif self.npuntos == 3:
-            self.lineEditFourLat.setText(str(mapPoint.x()))
-            self.lineEditFourLon.setText(str(mapPoint.y()))
+        print("Nice Click")
 
         # self.selectLayerCombo.clear()
         # you neet to set which layers will you identify here which is in your case is just 'parish_layer'
-        layer = None
-        for lyr in QgsMapLayerRegistry.instance().mapLayers().values():
-            if lyr.name() == "Electric Vehicle Demand":
-                layer = lyr
-                break
+        #layer = None
+        #for lyr in QgsMapLayerRegistry.instance().mapLayers().values():
+        #    if lyr.name() == "Electric Vehicle Demand":
+        #        layer = lyr
+        #        break
 
-        features = QgsMapToolIdentify(self.canvas).identify(mapPoint.x(), mapPoint.y(), layer,
-                                                                           QgsMapToolIdentify.TopDownStopAtFirst)
+        #features = QgsMapToolIdentify(self.canvas).identify(mapPoint.x(), mapPoint.y(), layer,
+        #                                                                   QgsMapToolIdentify.TopDownStopAtFirst)#
 
-        if len(features) > 0:
+        #if len(features) > 0:
             # here you get the selected feature
-            feature = features[0].mFeature
+            #feature = features[0].mFeature
             # And here you get the attribute's value
-            self.Demand[self.npuntos] = feature["Demand"]
-            print(str(feature["Demand"]))
-        x = mapPoint.x()
-        y = mapPoint.y()
-        point = self.canvas.getCoordinateTransform().toMapCoordinates(x, y)
+            #self.Demand[self.npuntos] = feature["Demand"]
+            #print(str(feature["Demand"]))
+        #x = mapPoint.x()
+        #y = mapPoint.y()
+        #point = self.canvas.getCoordinateTransform().toMapCoordinates(x, y)
 
 
         #Get the click
